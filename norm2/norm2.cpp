d0998e03b9853955e83adc81e0c4850e065bf228
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-02-20 04:02:18 +0300

    mita

--- norm2/norm2.cpp
+++ norm2/norm2.cpp
@@ -11,7 +11,7 @@ const int SIZE = 4;
 float norm(float[]);
 
 int main() {
-    float x[SIZE] = { 3.0, -4.5, 2.1, -6.7 }; 
+    float x[SIZE] = { 3.0, -4.5, 2.1, -6.7 };
     cout << "Norm of the array: " << norm(x) << endl;
 
     return EXIT_SUCCESS;
@@ -20,8 +20,8 @@ int main() {
 float norm(float arr[]) {
     float sum = 0.0;
     for (int i = 0; i < SIZE; ++i) {
-        sum += arr[i] * arr[i]; 
+        sum += arr[i] * arr[i];
     }
 
-    return sqrt(sum);  
+    return sqrt(sum);
 }
