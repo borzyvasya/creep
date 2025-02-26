3eb9b533629685647a5386896adcc88936a7059c
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-02-26 16:13:53 +0300

    another fixes of norms

--- nokotan/norm.cpp
+++ nokotan/norm.cpp
@@ -7,25 +7,36 @@ using namespace std;
 
 const int SIZE = 4;
 
-// L1
+float normL1(float[]); // L1
+float normL2(float[]); // L2
+float l_inf_norm(float[]); // L inf for max
+
+int main() {
+    float x[SIZE] = { 7.1, 5.4, 6.8, 4.5 };
+
+    cout << "Norm of the array (L1): " << normL1(x) << endl;
+    cout << "Norm of the array (L2): " << normL2(x) << endl;
+    cout << "Norm of the array (Linf): " << l_inf_norm(x) << endl;
+
+    return EXIT_SUCCESS;
+}
+
 float normL1(float arr[]) {
     float sum = 0.0;
-    for (int i = 0; i < SIZE; ++i) {
+    for (int i = 0; i < SIZE; ++i)
         sum += abs(arr[i]); // Модуль каждого элемента
-    }
+
     return sum;
 }
 
-// L2
 float normL2(float arr[]) {
     float sum = 0.0;
-    for (int i = 0; i < SIZE; ++i) {
+    for (int i = 0; i < SIZE; ++i) 
         sum += arr[i] * arr[i]; // Квадраты элементов
-    }
+
     return sqrt(sum); // Квадратный корень из суммы квадратов
 }
 
-// L infinite for max
 float l_inf_norm(float arr[]) {
     float max_val = 0.0;
     for (int i = 0; i < SIZE; ++i) {
@@ -34,15 +45,4 @@ float l_inf_norm(float arr[]) {
     }
 
     return max_val;
-}
-
-int main() {
-    float x[SIZE] = { -1.0, 6.5, 3.4, 8.1 };
-
-    cout << "Norm of the array (L1): " << normL1(x) << endl;
-    cout << "Norm of the array (L2): " << normL2(x) << endl;
-    cout << "Norm of the array (Linf): " << l_inf_norm(x) << endl;
-
-
-    return EXIT_SUCCESS;
 }
