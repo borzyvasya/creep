64022a52692443bc909386cb39cee4cdd8201ba2
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-02-26 16:07:59 +0300

    fixfing norms

--- nokotan/norm.cpp
+++ nokotan/norm.cpp
@@ -6,19 +6,43 @@
 using namespace std;
 
 const int SIZE = 4;
-float norm(float[]);
 
-int main() {
-    float x[SIZE] = { 3.0, -4.5, 2.1, -6.7 };
-    cout << "Norm of the array: " << norm(x) << endl;
-
-    return EXIT_SUCCESS;
-}
-
-float norm(float arr[]) {
+// L1
+float normL1(float arr[]) {
     float sum = 0.0;
     for (int i = 0; i < SIZE; ++i) {
-        sum += abs(arr[i]);
+        sum += abs(arr[i]); // Модуль каждого элемента
     }
     return sum;
+}
+
+// L2
+float normL2(float arr[]) {
+    float sum = 0.0;
+    for (int i = 0; i < SIZE; ++i) {
+        sum += arr[i] * arr[i]; // Квадраты элементов
+    }
+    return sqrt(sum); // Квадратный корень из суммы квадратов
+}
+
+// L infinite for max
+float l_inf_norm(float arr[]) {
+    float max_val = 0.0;
+    for (int i = 0; i < SIZE; ++i) {
+        float abs_val = abs(arr[i]);
+        if (abs_val > max_val) max_val = abs_val;
+    }
+
+    return max_val;
+}
+
+int main() {
+    float x[SIZE] = { -1.0, 6.5, 3.4, 8.1 };
+
+    cout << "Norm of the array (L1): " << normL1(x) << endl;
+    cout << "Norm of the array (L2): " << normL2(x) << endl;
+    cout << "Norm of the array (Linf): " << l_inf_norm(x) << endl;
+
+
+    return EXIT_SUCCESS;
 }
