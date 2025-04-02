3e2eb061690e1b924c4eb19daf32d8a63aa92052
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-04-02 23:08:05 +0300

    made in sewers

--- zzzc/iters.cpp
+++ zzzc/iters.cpp
@@ -10,24 +10,56 @@ const int N = 10;
 
 using namespace std;
 
-void generateMatrix(double A[N][N], double b[N]) {
+void generateMatrix(double[][N], double[]);
+void checkDiagonalDominance(double[][N]);
+// Метод простых итераций с oneMKL (cblas_ddot)
+void simpleIteration(double[][N], double[], double[], double);
+// Проверка решения с oneMKL (cblas_dgemv)
+void checkSolution(double[][N], double[], double[]);
+
+
+int main() {
+    double A[N][N], b[N], x[N];
+
+    generateMatrix(A, b);
+
+    cout << fixed << setprecision(3) << "Matrix A and b: " << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << A[i][j] << "\t";
+        cout << "| " << b[i] << endl;
+    }
+
+    simpleIteration(A, b, x, 1e-6);
+
+    cout << "\nSolution: " << endl;
+    for (int i = 0; i < N; i++)
+        cout << setprecision(7) << "x[" << i << "] = " << x[i] << endl;
+
+    return EXIT_SUCCESS;
+}
+
+void generateMatrix(double A[][N], double b[]) {
     srand(time(0));
+    double CF = 1.0e04;
+
     for (int i = 0; i < N; i++) {
         double sum = 0;
         for (int j = 0; j < N; j++) {
             if (i != j) {
-                A[i][j] = rand() % 10 + 1;
+                A[i][j] = rand() / CF;
                 sum += fabs(A[i][j]);
             }
         }
-        A[i][i] = sum + (rand() % 10 + 1);
+        A[i][i] = sum + (rand() / CF);
     }
+
     for (int i = 0; i < N; i++) {
-        b[i] = rand() % 50 + 1;
+        b[i] = rand() / CF;
     }
 }
 
-bool checkDiagonalDominance(double A[N][N]) {
+void checkDiagonalDominance(double A[][N]) {
     for (int i = 0; i < N; i++) {
         double sum = 0;
         for (int j = 0; j < N; j++) {
@@ -36,14 +68,13 @@ bool checkDiagonalDominance(double A[N][N]) {
             }
         }
         if (fabs(A[i][i]) <= sum) {
-            return false;
+            cout << "Matrix does not satisfy diagonal dominance." << endl;
+            exit(1);
         }
     }
-    return true;
 }
 
-// Метод простых итераций с oneMKL (cblas_ddot)
-void simpleIteration(double A[N][N], double b[N], double x[N], double epsilon) {
+void simpleIteration(double A[][N], double b[], double x[], double epsilon) {
     double x_new[N];
     int iter = 0;
     double error = epsilon + 1;
@@ -51,13 +82,9 @@ void simpleIteration(double A[N][N], double b[N], double x[N], double epsilon) {
     for (int i = 0; i < N; i++)
         x[i] = 0;
 
-    if (!checkDiagonalDominance(A)) {
-        cout << "Matrix does not satisfy diagonal dominance.\n";
-        return;
-    }
 
     while (error > epsilon) {
-#pragma omp parallel for
+        #pragma omp parallel for
         for (int i = 0; i < N; i++) {
             // Используем cblas_ddot для вычисления A[i] * x
             double sum = cblas_ddot(N, &A[i][0], 1, x, 1);
@@ -66,19 +93,18 @@ void simpleIteration(double A[N][N], double b[N], double x[N], double epsilon) {
         }
 
         error = 0;
-#pragma omp parallel for reduction(max:error)
+        #pragma omp parallel for reduction(max:error)
         for (int i = 0; i < N; i++) {
             error = max(error, fabs(x_new[i] - x[i]));
             x[i] = x_new[i];
         }
 
         iter++;
-        cout << "Iteration " << iter << ", error: " << error << "\n";
+        cout << setprecision(7) << "Iteration " << iter << ", error: " << error << "\n";
     }
 }
 
-// Проверка решения с oneMKL (cblas_dgemv)
-void checkSolution(double A[N][N], double b[N], double x[N]) {
+void checkSolution(double A[][N], double b[], double x[]) {
     double Ax[N];
     cblas_dgemv(CblasRowMajor, CblasNoTrans, N, N, 1.0, &A[0][0], N, x, 1, 0.0, Ax, 1);
 
@@ -87,27 +113,4 @@ void checkSolution(double A[N][N], double b[N], double x[N]) {
         residual = max(residual, fabs(Ax[i] - b[i]));
     }
     cout << "Residual: " << residual << "\n";
-}
-
-int main() {
-    double A[N][N], b[N], x[N];
-
-    generateMatrix(A, b);
-
-    cout << "Matrix A and b:\n";
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
-            cout << A[i][j] << "\t";
-        cout << "| " << b[i] << "\n";
-    }
-
-    simpleIteration(A, b, x, 1e-6);
-
-    cout << "\nSolution:\n";
-    for (int i = 0; i < N; i++)
-        cout << setprecision(10) << fixed << "x[" << i << "] = " << x[i] << "\n";
-
-    checkSolution(A, b, x);
-
-    return 0;
 }
